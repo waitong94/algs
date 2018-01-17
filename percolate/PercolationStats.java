e6aa47da6cc35eeb4d41fa8b55e7ff3203d30ae1
waitong94 <waitong94@gmail.com>
2018-01-17 13:48:08 +0000

    Stats mean completed

--- percolate/PercolationStats.java
+++ percolate/PercolationStats.java
@@ -1,16 +1,19 @@
 import edu.princeton.cs.algs4.StdRandom;
 import edu.princeton.cs.algs4.StdStats;
 import edu.princeton.cs.algs4.StdOut;
-import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.StdIn;
 
 public class PercolationStats {
    private Percolation perc;
    private double[] openSites;
+   private int n;
+   private static double meanValue;
    public PercolationStats(int n, int trials)    // perform trials independent experiments on an n-by-n grid
    {
        if (n<=0 || trials<=0)
            throw new IllegalArgumentException();
        StdRandom.setSeed(1);
+       this.n=n;
        openSites=new double[trials];
        for (int it=0;it<trials;it++)
        {       
@@ -22,13 +25,14 @@ public class PercolationStats {
                perc.open(i,j);//open the site
            }
            openSites[it]=perc.numberOfOpenSites();
+
        }
-       
+       this.meanValue=mean();
    }
    
    public double mean()                          // sample mean of percolation threshold
    {
-       return StdStats.mean(openSites);
+       return StdStats.mean(openSites)/(n*n);
    }
 //   public double stddev()                        // sample standard deviation of percolation threshold
 //   public double confidenceLo()                  // low  endpoint of 95% confidence interval
@@ -36,8 +40,11 @@ public class PercolationStats {
 //
    public static void main(String[] args)        // test client (described below)
    {
-       In in = new In(args[1]);      // input file
-       StdOut.println("Mean: " + PercolationStats.mean());
+          // input file
+       int n=Integer.valueOf(args[0]);
+       int trials=Integer.valueOf(args[1]);
+       PercolationStats percStats= new PercolationStats(n,trials);
+       StdOut.println("Mean: " + PercolationStats.meanValue);
 //       StdOut.println("Standard Deviation: " + PercolationStats.stddev());
 //       StdOut.println("Mean: " + PercolationStats.confidenceLo());
 //       StdOut.println("Mean: " + PercolationStats.confidenceHi());
